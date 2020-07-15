Havoc-OS/android_packages_apps_HavocSettings

Add seekbar and list settings preferences
Taken from AICP

Change-Id: Ibe3454b7841e39487529a4004727cd941c4c72fd

src/com/havoc/settings/preferences/LongClickablePreference.java
@@ -1,0 +1,137 @@
+/*
+ * Copyright (C) 2017 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.havoc.settings.preferences;
+
+import android.content.Context;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceViewHolder;
+import android.os.Handler;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class LongClickablePreference extends Preference {
+
+    private Handler mHandler = new Handler();
+    private boolean mAllowNormalClick;
+    private boolean mAllowBurst;
+
+    private int mClickableViewId = 0;
+    private int mLongClickDurationMillis;
+    private int mLongClickBurstMillis = 0;
+    private PreferenceViewHolder mViewHolder;
+    private Preference.OnPreferenceClickListener mClickListener;
+    private Preference.OnPreferenceClickListener mLongClickListener;
+
+    public LongClickablePreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public LongClickablePreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public LongClickablePreference(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+        mViewHolder = holder;
+
+        setupClickListeners();
+    }
+
+    @Override
+    public void setOnPreferenceClickListener(
+            Preference.OnPreferenceClickListener onPreferenceClickListener) {
+        mClickListener = onPreferenceClickListener;
+
+        setupClickListeners();
+    }
+
+    public void setOnLongClickListener(int viewId, int longClickDurationMillis,
+            Preference.OnPreferenceClickListener onPreferenceClickListener) {
+        mClickableViewId = viewId;
+        mLongClickDurationMillis = longClickDurationMillis;
+        mLongClickListener = onPreferenceClickListener;
+
+        setupClickListeners();
+    }
+
+    private Runnable mLongClickRunnable = new Runnable() {
+            @Override
+            public void run() {
+                mAllowNormalClick = false;
+                mLongClickListener.onPreferenceClick(LongClickablePreference.this);
+                if (mAllowBurst && mLongClickBurstMillis > 0) {
+                    mHandler.postDelayed(this, mLongClickBurstMillis);
+                }
+            }
+    };
+
+    public void setLongClickBurst(int intervalMillis) {
+        mLongClickBurstMillis = intervalMillis;
+    }
+
+    private void setupClickListeners() {
+        // We can't put long click listener on our view without sacrificing default
+        // preference click functionality, so detect long clicks manually with touch listener
+        if (mClickableViewId != 0 && mViewHolder != null) {
+            View view = mViewHolder.findViewById(mClickableViewId);
+            if (view != null) {
+                view.setOnTouchListener(new View.OnTouchListener() {
+                        @Override
+                        public boolean onTouch(View v, MotionEvent event) {
+                            switch (event.getActionMasked()) {
+                                case MotionEvent.ACTION_DOWN:
+                                    mAllowNormalClick = true;
+                                    mAllowBurst = true;
+                                    mHandler.postDelayed(mLongClickRunnable,
+                                            mLongClickDurationMillis);
+                                    break;
+                                case MotionEvent.ACTION_UP:
+                                    mHandler.removeCallbacks(mLongClickRunnable);
+                                    mAllowBurst = false;
+                                    break;
+                            }
+                            return false;
+                        }
+                });
+            }
+        }
+        // Use our own preference click listener to handle both normal and long clicks
+        if (getOnPreferenceClickListener() == null) {
+            super.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                    @Override
+                    public boolean onPreferenceClick(Preference preference) {
+                        mAllowBurst = false;
+                        mHandler.removeCallbacks(mLongClickRunnable);
+                        if (mAllowNormalClick) {
+                            return mClickListener != null &&
+                                    mClickListener.onPreferenceClick(preference);
+                        } else {
+                            // Long press done
+                            return true;
+                        }
+                    }
+
+            });
+        }
+    }
+}

src/com/havoc/settings/preferences/SecureSettingListPreference.java
@@ -1,0 +1,49 @@
+/*
+ * Copyright (C) 2017-2018 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.havoc.settings.preferences;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+
+public class SecureSettingListPreference extends ListPreference {
+
+    public SecureSettingListPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+    }
+
+    public SecureSettingListPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+    }
+
+    public SecureSettingListPreference(Context context) {
+        super(context);
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+    }
+
+    @Override
+    protected void onSetInitialValue(boolean restoreValue, Object defaultValue) {
+        // This is what default ListPreference implementation is doing without respecting
+        // real default value:
+        //setValue(restoreValue ? getPersistedString(mValue) : (String) defaultValue);
+        // Instead, we better do
+        setValue(restoreValue ? getPersistedString((String) defaultValue) : (String) defaultValue);
+    }
+
+}

src/com/havoc/settings/preferences/SystemSettingEditTextPreference.java
@@ -1,0 +1,68 @@
+/*
+ * Copyright (C) 2017 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.havoc.settings.preferences;
+
+import android.content.Context;
+import android.support.v7.preference.EditTextPreference;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+
+public class SystemSettingEditTextPreference extends EditTextPreference {
+    private boolean mAutoSummary = false;
+
+    public SystemSettingEditTextPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+
+    public SystemSettingEditTextPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+
+    public SystemSettingEditTextPreference(Context context) {
+        super(context);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+
+    @Override
+    protected void onSetInitialValue(boolean restoreValue, Object defaultValue) {
+        // This is what default ListPreference implementation is doing without respecting
+        // real default value:
+        //setText(restoreValue ? getPersistedString(mText) : (String) defaultValue);
+        // Instead, we better do
+        setText(restoreValue ? getPersistedString((String) defaultValue) : (String) defaultValue);
+    }
+
+    @Override
+    public void setText(String text) {
+        super.setText(text);
+        if (mAutoSummary || TextUtils.isEmpty(getSummary())) {
+            setSummary(text, true);
+        }
+    }
+
+    @Override
+    public void setSummary(CharSequence summary) {
+        setSummary(summary, false);
+    }
+
+    private void setSummary(CharSequence summary, boolean autoSummary) {
+        mAutoSummary = autoSummary;
+        super.setSummary(summary);
+    }
+}

src/com/havoc/settings/preferences/SystemSettingIntListPreference.java
@@ -1,0 +1,46 @@
+/*
+ * Copyright (C) 2017 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.havoc.settings.preferences;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class SystemSettingIntListPreference extends SystemSettingListPreference {
+
+    public SystemSettingIntListPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public SystemSettingIntListPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SystemSettingIntListPreference(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected boolean persistString(String value) {
+        return persistInt(Integer.parseInt(value));
+    }
+
+    @Override
+    protected String getPersistedString(String defaultReturnValue) {
+        return String.valueOf(getPersistedInt(Integer.parseInt(defaultReturnValue)));
+    }
+
+}

src/com/havoc/settings/preferences/SystemSettingSeekBarPreference.java
@@ -1,0 +1,38 @@
+/*
+ * Copyright (C) 2017 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.havoc.settings.preferences;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class SystemSettingSeekBarPreference extends SeekBarPreferenceCham {
+
+    public SystemSettingSeekBarPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+
+    public SystemSettingSeekBarPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+
+    public SystemSettingSeekBarPreference(Context context) {
+        super(context, null);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+}

op5-q/android_packages_apps_NitrogenSettings

Signed-off-by: spezi77 <spezi7713@gmx.net>
Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

src/com/nitrogen/settings/preferences/LongClickablePreference.java
@@ -1,0 +1,138 @@
+/*
+ * Copyright (C) 2017 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.nitrogen.settings.preferences;
+
+import android.content.Context;
+import android.os.Handler;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceViewHolder;
+
+public class LongClickablePreference extends Preference {
+
+    private Handler mHandler = new Handler();
+    private boolean mAllowNormalClick;
+    private boolean mAllowBurst;
+
+    private int mClickableViewId = 0;
+    private int mLongClickDurationMillis;
+    private int mLongClickBurstMillis = 0;
+    private PreferenceViewHolder mViewHolder;
+    private Preference.OnPreferenceClickListener mClickListener;
+    private Preference.OnPreferenceClickListener mLongClickListener;
+
+    public LongClickablePreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public LongClickablePreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public LongClickablePreference(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+        mViewHolder = holder;
+
+        setupClickListeners();
+    }
+
+    @Override
+    public void setOnPreferenceClickListener(
+            Preference.OnPreferenceClickListener onPreferenceClickListener) {
+        mClickListener = onPreferenceClickListener;
+
+        setupClickListeners();
+    }
+
+    public void setOnLongClickListener(int viewId, int longClickDurationMillis,
+            Preference.OnPreferenceClickListener onPreferenceClickListener) {
+        mClickableViewId = viewId;
+        mLongClickDurationMillis = longClickDurationMillis;
+        mLongClickListener = onPreferenceClickListener;
+
+        setupClickListeners();
+    }
+
+    private Runnable mLongClickRunnable = new Runnable() {
+            @Override
+            public void run() {
+                mAllowNormalClick = false;
+                mLongClickListener.onPreferenceClick(LongClickablePreference.this);
+                if (mAllowBurst && mLongClickBurstMillis > 0) {
+                    mHandler.postDelayed(this, mLongClickBurstMillis);
+                }
+            }
+    };
+
+    public void setLongClickBurst(int intervalMillis) {
+        mLongClickBurstMillis = intervalMillis;
+    }
+
+    private void setupClickListeners() {
+        // We can't put long click listener on our view without sacrificing default
+        // preference click functionality, so detect long clicks manually with touch listener
+        if (mClickableViewId != 0 && mViewHolder != null) {
+            View view = mViewHolder.findViewById(mClickableViewId);
+            if (view != null) {
+                view.setOnTouchListener(new View.OnTouchListener() {
+                        @Override
+                        public boolean onTouch(View v, MotionEvent event) {
+                            switch (event.getActionMasked()) {
+                                case MotionEvent.ACTION_DOWN:
+                                    mAllowNormalClick = true;
+                                    mAllowBurst = true;
+                                    mHandler.postDelayed(mLongClickRunnable,
+                                            mLongClickDurationMillis);
+                                    break;
+                                case MotionEvent.ACTION_UP:
+                                    mHandler.removeCallbacks(mLongClickRunnable);
+                                    mAllowBurst = false;
+                                    break;
+                            }
+                            return false;
+                        }
+                });
+            }
+        }
+        // Use our own preference click listener to handle both normal and long clicks
+        if (getOnPreferenceClickListener() == null) {
+            super.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                    @Override
+                    public boolean onPreferenceClick(Preference preference) {
+                        mAllowBurst = false;
+                        mHandler.removeCallbacks(mLongClickRunnable);
+                        if (mAllowNormalClick) {
+                            return mClickListener != null &&
+                                    mClickListener.onPreferenceClick(preference);
+                        } else {
+                            // Long press done
+                            return true;
+                        }
+                    }
+
+            });
+        }
+    }
+}

src/com/nitrogen/settings/preferences/SecureSettingListPreference.java
@@ -1,0 +1,49 @@
+/*
+ * Copyright (C) 2017-2018 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.nitrogen.settings.preferences;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+
+public class SecureSettingListPreference extends ListPreference {
+
+    public SecureSettingListPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+    }
+
+    public SecureSettingListPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+    }
+
+    public SecureSettingListPreference(Context context) {
+        super(context);
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+    }
+
+    @Override
+    protected void onSetInitialValue(boolean restoreValue, Object defaultValue) {
+        // This is what default ListPreference implementation is doing without respecting
+        // real default value:
+        //setValue(restoreValue ? getPersistedString(mValue) : (String) defaultValue);
+        // Instead, we better do
+        setValue(restoreValue ? getPersistedString((String) defaultValue) : (String) defaultValue);
+    }
+
+}

src/com/nitrogen/settings/preferences/SystemSettingEditTextPreference.java
@@ -1,0 +1,69 @@
+/*
+ * Copyright (C) 2017 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.nitrogen.settings.preferences;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+
+import androidx.preference.EditTextPreference;
+
+public class SystemSettingEditTextPreference extends EditTextPreference {
+    private boolean mAutoSummary = false;
+
+    public SystemSettingEditTextPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+
+    public SystemSettingEditTextPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+
+    public SystemSettingEditTextPreference(Context context) {
+        super(context);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+
+    @Override
+    protected void onSetInitialValue(boolean restoreValue, Object defaultValue) {
+        // This is what default ListPreference implementation is doing without respecting
+        // real default value:
+        //setText(restoreValue ? getPersistedString(mText) : (String) defaultValue);
+        // Instead, we better do
+        setText(restoreValue ? getPersistedString((String) defaultValue) : (String) defaultValue);
+    }
+
+    @Override
+    public void setText(String text) {
+        super.setText(text);
+        if (mAutoSummary || TextUtils.isEmpty(getSummary())) {
+            setSummary(text, true);
+        }
+    }
+
+    @Override
+    public void setSummary(CharSequence summary) {
+        setSummary(summary, false);
+    }
+
+    private void setSummary(CharSequence summary, boolean autoSummary) {
+        mAutoSummary = autoSummary;
+        super.setSummary(summary);
+    }
+}

src/com/nitrogen/settings/preferences/SystemSettingIntListPreference.java
@@ -1,0 +1,46 @@
+/*
+ * Copyright (C) 2017 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.nitrogen.settings.preferences;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class SystemSettingIntListPreference extends SystemSettingListPreference {
+
+    public SystemSettingIntListPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public SystemSettingIntListPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SystemSettingIntListPreference(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected boolean persistString(String value) {
+        return persistInt(Integer.parseInt(value));
+    }
+
+    @Override
+    protected String getPersistedString(String defaultReturnValue) {
+        return String.valueOf(getPersistedInt(Integer.parseInt(defaultReturnValue)));
+    }
+
+}

src/com/nitrogen/settings/preferences/SystemSettingSeekBarPreference.java
@@ -1,0 +1,38 @@
+/*
+ * Copyright (C) 2017 AICP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.nitrogen.settings.preferences;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class SystemSettingSeekBarPreference extends SeekBarPreferenceCham {
+
+    public SystemSettingSeekBarPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+
+    public SystemSettingSeekBarPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+
+    public SystemSettingSeekBarPreference(Context context) {
+        super(context, null);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+}